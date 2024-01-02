2c1a236c59ab94f92a5e4627f8768ddd4fd39a18
Parker Macdonald <larrythepig123@gmail.com>
2024-01-01 23:02:05 -0700

    day 1, part 1 working

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# adventof-C-ode
-Advent of Code 2023 in C.
+# Advent of Code in C
+
+## Day 1
+
+### Part 1:
+
+```bash
+cd day-1/
+
+gcc -o 1 -O3 1.c
+
+./1 input.txt
+```
---
+++ day-1/1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("No file provided.\n");
+        return 1;
+    }
+
+    FILE *pF = fopen(argv[1], "r");
+    char buffer[255];
+
+    if (pF == NULL) {
+        printf("Unable to open file.\n");
+        return 1;
+    }
+
+    int sum = 0;
+
+    while(fgets(buffer, 255, pF) != NULL) {
+        int firstNum = 0;
+        int finalNum = 0;
+        
+        for (int i = 0; i < strlen(buffer); i++) {
+            char current = buffer[i];
+            
+            int x = current - '0';
+
+            // check if x is properly bounded
+            if (x >= 0 && x <= 9) {
+                if (firstNum == 0) {
+                    firstNum = x;
+                }
+                finalNum = x;
+            }
+        }
+
+        sum += firstNum * 10 + finalNum;
+    }
+
+    printf("%d\n", sum);
+
+    fclose(pF);
+
+    return 0;
+}
